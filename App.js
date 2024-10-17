eaef1e3097e4866760a5942995bba1a3308b533c
stefleon33 <158505324+stefleon33@users.noreply.github.com>
2024-10-17 12:14:04 -0400

    First commit

--- App.js
+++ App.js
@@ -1,20 +1,47 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+// import react Navigation
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
-export default function App() {
+// Create the navigator
+const Stack = createNativeStackNavigator();
+
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+
+// import the screens
+import ShoppingLists from './components/ShoppingLists';
+import Welcome from './components/Welcome';
+
+const App = () => {
+const firebaseConfig = {
+  apiKey: "AIzaSyBFZpYiEeeHFWf5146O88aoV_0wXuf_m7E",
+  authDomain: "shopping-list-demo-c67b3.firebaseapp.com",
+  projectId: "shopping-list-demo-c67b3",
+  storageBucket: "shopping-list-demo-c67b3.appspot.com",
+  messagingSenderId: "648476402064",
+  appId: "1:648476402064:web:bdbfdf02624bbceb555315"
+};
+
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
+
+  // Initialize Cloud Firestore and get a reference to the service
+  const db = getFirestore(app);
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+        <NavigationContainer>
+      <Stack.Navigator
+        initialRouteName="Welcome"
+      >
+        <Stack.Screen name="Welcome" component={Welcome} />
+        <Stack.Screen
+          name="ShoppingLists"
+        >
+          {props => <ShoppingLists db={db} {...props} />}
+        </Stack.Screen>
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+export default App;
